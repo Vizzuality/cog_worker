614d9c86e2b928539ea3f732a7af9b53b4a673ac
fgassert <fgassert@gmail.com>
2021-08-18 14:36:38 -0500

    bumpversion config

--- cog_worker/__init__.py
+++ cog_worker/__init__.py
@@ -4,6 +4,8 @@ COG Worker is a simple library to help you chunk and run large scale analysis
 on Cloud Optimized GeoTIFFs (COGS).
 """
 
+__version__ = "0.1.0"
+
 from .worker import Worker  # noqa
 from .manager import Manager  # noqa
 from .types import *  # noqa
