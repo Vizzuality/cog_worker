190967849710fad0537101fb3832c6088e039fbf
fgassert <fgassert@gmail.com>
2023-07-13 21:52:53 -0500

    Bump version: 0.1.4 → 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="cog_worker",
-    version="0.1.4",
+    version="0.2.0",
     description="Scalable geospatial analysis on Cloud Optimized GeoTIFFs.",
     long_description=desc,
     long_description_content_type="text/markdown",
--- sphinx_docs/source/conf.py
+++ sphinx_docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Simbiotica S.L.'
 author = 'Francis Gassert'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.4'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
